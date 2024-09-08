bee29d388ca5e0fad80f25a7cc837006c52be772
kangjjs <emdtod12@naver.com>
2024-09-08 16:45:14 +0900

    LabdaEx2

---
+++ src/LamdaAndStream/LambdaEx2.java
@@ -0,0 +1,14 @@
+package LamdaAndStream;
+
+public class LambdaEx2 {
+
+    public static void main(String[] args) {
+        MyFunction f = () -> {};
+        Object obj = (MyFunction) (() -> {});
+        String str = ((Object)(MyFunction) (()->{})).toString();
+
+        System.out.println(f);
+        System.out.println(obj);
+        System.out.println(str);
+    }
+}
